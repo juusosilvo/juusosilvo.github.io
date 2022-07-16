f028592f9b6ec661c03a0d1580635c3e0957e12f
Juuso Silvo <106491133+juusosilvo@users.noreply.github.com>
2022-07-16 19:44:35 +0300

    a

--- assets/js/video.js
+++
@@ -1,9 +0,0 @@
-
-    var autoPlayVideo = document.getElementById("bvideo");
-    autoPlayVideo.oncanplaythrough = function() {
-        autoPlayVideo.muted = true;
-        autoPlayVideo.play();
-        autoPlayVideo.pause();
-        autoPlayVideo.play();
-        autoPlayVideo.muted = false;
-    }
